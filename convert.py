4fe32213f3a966f1ab858ead76ade7cddfedf97b
Joss t <jossthomas@onetel.com>
2015-11-25 17:57:13 +0000

    Initial Commit Part 2

--- convert.py
+++ convert.py
@@ -3,48 +3,15 @@ import datetime as dt
 import random
 from PIL import Image, ImageFont, ImageDraw
 
-def wrap_text(string):
-    '''split text into equal length strings without splitting words and pad'''
-    space_indexes = []
-    lines =  []
-    start = 0
-    step_init = 981
-    target = step_init + 10
-    step = step_init
-
-    for i, letter in enumerate(string): #find all the spaces
-        if letter == ' ':
-            space_indexes.append(i)
-
-    while start + step < len(string):
-        while start + step not in space_indexes:
-            step += 1
-        line = string[start:start+step].lstrip() #Cut line and make sure there are no starting spaces
-
-        spaces_add = target - len(line)
-        line2 = ''
-        
-        for i in line: #try and make sure line is target length
-            if i == ' ' and spaces_add > 0 and random.randrange(0,6) == 0: #randomness to prevent stacking spaces
-                line2 += '  '
-                spaces_add -= 1
-            else:
-                line2 += i
-
-        lines.append(line2) #put newlines approximately every step_init chars
-        start, step = start + step + 1, step_init #step + 1 to remove the preceding space
-    lines.append(string[start:])
-
-    return lines
-
 def clean_text(string):
+    '''Just a bunch of unreadable regex to clean up the original NASA recording'''
     string_1 = re.sub('\f.*\n|Tape.*\n|\(.*\)\n|^\d*|\n\d*\n|\f|.*END OF TAPE.*\n|# # #|.*AIR-TO-GROUND.*\n|.*Three asterisks denote.*\n|\n\d*\n', '', string) #remove a lot of NASA notes
     string_2 = re.sub('.* (?=\d{2} \d{2} \d{2} \d{2})','', string_1) #remove comments preceding timestamps
     string_3 = re.sub('\n\d*\.\n|\n399*','', string_2) #remove numbers from lines
     string_4 = re.sub('\n(?!\d{2} \d{2} \d{2} \d{2})',' ', string_3) #Remove a newline not followed by a timestamp.
     string_5 = re.sub('\n\d{2} \d{2} \d{2} \d{2} [A-Z]{2,3}\n','', string_4) #remove any  orphaned timestamps
     string_6 = string_5.replace('  ', ' ').replace(' .', '.').replace('- -', '').replace('....', '...') #Easier than dealing with . as a special char
-    string_7 = string_6.split('\n')
+    string_7 = string_6.replace('--', '-').replace('***', '*').split('\n') # need to get rid of 1 line!
 
     return string_7
 
@@ -59,17 +26,69 @@ def set_timestamp(text, launch_date):
             outstr += line
     return outstr
 
-def draw_poster(poster_text, textsize):
-    font = ImageFont.truetype("NotCourierSans.otf", textsize) #This font needs to be monopaced!
-    im = Image.new("RGBA", (9922, 14036), "black")
-    draw = ImageDraw.Draw(im)
-    for i, text in enumerate(poster_text):
-        if "That's  one small step for man, one giant leap for mankind." in text:
-            #print(text)
-            draw.text((4, int((i + 3/16) * textsize)), text, font=font, fill=(255,0,0,255))
-        else:
-            draw.text((4, int((i + 3/16) * textsize)), text, font=font, fill=(255,255,255,255))
+def wrap_text(string):
+    '''split text into equal length strings without splitting words and pad'''
+    length = len(string)
+    space_indexes = []
+    lines =  []
+    start = 0
+    step_init = 985
+    target = step_init + 6 #As spaces after words are considered target needs to be step + longest word
+    step = step_init
 
+    for i, letter in enumerate(string): #find all the spaces
+        if letter == ' ':
+            space_indexes.append(i)
+    length_spaces = len(space_indexes)
+
+    print('Found all spaces')
+
+    while start + step < len(string):
+        #Create a sliding search from as indexing space list is way too slow due to length - actually works somehow!
+        current_percentage = round(start / length, 3)
+        frame_start = int(max(0, (current_percentage - 0.005) * length_spaces))
+        frame_end = int(max(0, (current_percentage + 0.005) * length_spaces))
+        frame = space_indexes[frame_start:frame_end]
+        assert start == 0  or frame[0] < start < frame[-1], "Reading frame misaligned! Position: {0[0]}, \
+        Frame Start: {0[1]}, Frame End: {0[2]}".format([int(current_percentage * length), frame[0], frame[-1]])
+
+        step = min(frame, key=lambda x:abs(x-(start + step))) - start #find closest space to desired line end
+        line = string[start:start+step].lstrip() #Cut line and make sure there are no starting spaces
+        line = pad_text(line, target)
+        lines.append(line) #make a list of uniform length lines
+        start, step = start + step + 1, step_init #step + 1 to remove the preceding space
+    lines.append(string[start:]) #add whatever is left over in the string 
+    return lines
+
+def pad_text(text, target):
+    '''Pad lines to required length using random(ish) space insertion'''
+    spaces_req = target - len(text)
+    line = ''
+    for i in text: #make sure line is target length
+        if i == ' ' and spaces_req > 0 and random.randrange(0,6) == 0: #randomness to prevent stacking spaces
+            line += '  '
+            spaces_req -= 1
+        else:
+            line += i
+    return line
+
+def draw_poster(poster_text, textsize):
+    '''split out and highlight the words'''
+    font = ImageFont.truetype("NotCourierSans.otf", textsize) #This font needs to be monopaced!
+    im = Image.new("RGBA", (9922, 14036), "black") #A1 Size
+    draw = ImageDraw.Draw(im) #Set up sheet to draw on
+    for i, text in enumerate(poster_text):
+        if "1969-07-21 02:56:48 CDR" in text:
+            quote = "1969-07-21 02:56:48 CDR (TRANQ) That's one small step for man, one giant leap for mankind."
+            text = text.split(quote)
+            print(text[0], '\n', text[1])
+            width_p1, h1 = draw.textsize(text[0], font=font)
+            width_quote, h2 = draw.textsize(quote, font=font)
+            draw.text((4, int((i + 3/8) * textsize)), text[0], font=font, fill=(255,255,255,255)) #All text padded 4 pixels left
+            draw.text((width_p1, int((i + 3/8) * textsize)), quote, font=font, fill=(255,0,0,255)) 
+            draw.text((width_p1 + width_quote, int((i + 3/8) * textsize)), text[1], font=font, fill=(255,255,255,255))
+        else:
+            draw.text((4, int((i + 3/8) * textsize)), text, font=font, fill=(255,255,255,255))
     im.save("output.png", "PNG")
 
 def output_text(text):
@@ -82,8 +101,14 @@ launch_date = dt.datetime(1969, 7, 16, 13, 32, 0) # set the time to 16/07/1969 1
 textsize = 16
 
 infile = infile.read()
+print('Read infile')
 clean_infile = clean_text(infile)
+print('Cleaned infile')
 outstr = set_timestamp(clean_infile, launch_date)
+print('Set Timestamps')
 wrapped_outstr = wrap_text(outstr)
+print('Cut text into lines')
 output_text(wrapped_outstr)
+print('Saved text')
 draw_poster(wrapped_outstr, textsize)
+print('Poster finished!')
